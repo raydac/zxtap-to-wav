0f0a331f60fb2db5b8be37e65eb8c4de85818b95
Igor Maznitsa <rrg4400@gmail.com>
2025-11-09 12:38:28 +0200

    restructured for wasm support

---
+++ src/wasm_main.go
@@ -0,0 +1,17 @@
+//go:build js || wasm
+
+package main
+
+import (
+    "syscall/js"
+)
+
+func ProcessFile(this js.Value, args []js.Value) interface{} {
+    return nil
+}
+
+func main() {
+	js.Global().Set("processFile", js.FuncOf(ProcessFile))
+	println("WASM Go runtime started")
+	select {}
+}
